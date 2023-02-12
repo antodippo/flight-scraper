221f7be48664d4dfc68c144392b4e13f1de2d331
arsalan77x <63359673+arsalan77x@users.noreply.github.com>
2023-02-12 18:07:24 +0330

    issues 3 and 11 (#20)
    * validations added.
    
    * store queries as json.
    
    * store queries in json fixed
    
    * validation added
    
    * multiple emails validation added.
    
    * mutiple emails validation
    
    * store queries independently

--- ParseFlags.go
+++ ParseFlags.go
@@ -2,7 +2,12 @@ package main
 
 import (
 	"flag"
+	"io/ioutil"
+	"net/http"
 	"os"
+	"regexp"
+	"strings"
+	"time"
 )
 
 // ParseFlags parses input flags
@@ -15,10 +20,59 @@ func ParseFlags(progname string, args []string) (searchInput SearchInput, recipi
 	flags.Parse(args)
 
 	if *departurePtr == "" || *arrivalPtr == "" || *datePtr == "" || *recipientPtr == "" {
-		LogWarning("Not enough parameters:")
-		flags.PrintDefaults()
-		os.Exit(0)
+		handleWarning(flags, "Not enough parameters:")
+	}
+	if !isIATAValid(departurePtr) || !isIATAValid(arrivalPtr) {
+		handleWarning(flags, "Invalid IATA:")
+	}
+	if !isDateValid(datePtr) {
+		handleWarning(flags, "Invalid date format:")
+	}
+	if !IsEmailValid(recipientPtr) {
+		handleWarning(flags, "Invalid email format:")
 	}
 
 	return SearchInput{*departurePtr, *arrivalPtr, *datePtr}, *recipientPtr
+}
+
+func handleWarning(flags *flag.FlagSet, message string) {
+	LogWarning(message)
+	flags.PrintDefaults()
+	os.Exit(0)
+}
+
+func isDateValid(datePtr *string) bool {
+	_, err := time.Parse("2006-01-02", *datePtr)
+	if err != nil {
+		return false
+	} else {
+		return true
+	}
+}
+
+func isIATAValid(departurePtr *string) bool {
+	//api_key has only 1000 requesst available per month.
+	var api_key string = "b6a88e00-390d-4187-930f-550ea564f9b3"
+	url := "https://airlabs.co/api/v9/airports.csv?iata_code=" + *departurePtr + "&api_key=" + api_key
+	req, _ := http.NewRequest("GET", url, nil)
+	res, _ := http.DefaultClient.Do(req)
+	defer res.Body.Close()
+	body, _ := ioutil.ReadAll(res.Body)
+	if string(body) == "" {
+		return false
+	} else {
+		return true
+	}
+}
+
+func IsEmailValid(email *string) bool {
+	emailPattern := regexp.MustCompile(`^[A-Za-z0-9]+[@]+[A-Za-z0-9]+[.]+com$`)
+	emailsToList := strings.Split(*email, ",")
+	for _, e := range emailsToList {
+		if !emailPattern.MatchString(e) || len(e) > 50 {
+			return false
+		}
+	}
+	return true
+
 }
--- StoreJsonResults.go
+++ StoreJsonResults.go
@@ -5,9 +5,30 @@ import (
 	"io/ioutil"
 )
 
+type Query struct {
+	Departure     string
+	Arrival       string
+	DepartureTime string
+	Email         string
+}
+
 // StoreJSONResults stores a list of results in a json file
 func StoreJSONResults(results []FlightResult, filename string) {
 	jsonString, err := json.MarshalIndent(results, "", "\t")
+	LogErrorIfNotNull(err)
+	err = ioutil.WriteFile(filename, jsonString, 0644)
+	LogErrorIfNotNull(err)
+}
+
+// StoreJSONQueries stores a list of queries in a json file
+func StoreJSONQueries(searchInput SearchInput, recipient string, filename string) {
+	query := Query{
+		Departure:     searchInput.Departure,
+		Arrival:       searchInput.Arrival,
+		DepartureTime: searchInput.Date,
+		Email:         recipient,
+	}
+	jsonString, err := json.MarshalIndent(query, "", "\t")
 	LogErrorIfNotNull(err)
 	err = ioutil.WriteFile(filename, jsonString, 0644)
 	LogErrorIfNotNull(err)
